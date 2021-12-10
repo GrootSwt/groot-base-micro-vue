b72ba9f9342f1fae9d1a6fcd7747f0bfdbc73abd
swt <396518038@qq.com>
2021-12-10 14:55:20 +0800

    增加文件上传路径

--- src/api/oss.js
+++ src/api/oss.js
@@ -2,6 +2,7 @@ import { deleteRequest } from '@/plugins/request'
 
 const ROOT_PATH = '/micro-oss/'
 
+export const UPLOAD_URL = ROOT_PATH + 'fileOperation/upload'
 // 根据文件信息id删除文件
 export function deleteFileById (data) {
   return deleteRequest(ROOT_PATH + `fileOperation/${data.id}/deleteFileById`)
